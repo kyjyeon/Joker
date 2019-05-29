021ad3d2101c67f513eec164cbc4946ff41f3a4f
kyjyeon <kyjyeon93@gmail.com>
2019-05-29 17:17:03 +0900

    fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const route = require('./Routers/route');
 const mongoose = require('mongoose');
 //Before activating must connect to mongodb to interface
 //Connecting code [mongod --dbpath=data --bind_ip 127.0.0.1] at mongodb directory
-mongoose.connect('mongodb://27017/Joker', {useNewUrlParser: true})
-.then(()=>console.log('mongodb connected'))
-.catch(err => console.log(err));
+// mongoose.connect('mongodb://27017/Joker', {useNewUrlParser: true})
+// .then(()=>console.log('mongodb connected'))
+// .catch(err => console.log(err));
 
 route.startbot();
