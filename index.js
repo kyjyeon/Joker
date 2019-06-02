aacdd2b42925e3ad862c1a4985e2536cfcfe71d1
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2019-06-03 02:03:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const route = require('./Routers/route');
 const mongoose = require('mongoose');
 const createdb = require('./db_control');
-//Before activating must connect to mongodb to interface
-//Connecting code [mongod --dbpath=data --bind_ip 127.0.0.1] at mongodb directory
 
-// mongoose.connect('mongodb://27017/', {useNewUrlParser: true})
-// .then(()=>console.log('mongodb connected'))
-// .catch(err => console.log(err));
-createdb.createdb();
 route.startbot();
