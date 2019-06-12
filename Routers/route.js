35c88191960b600c375d0dcec8bb6e25228fa60d
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2019-06-13 02:05:17 +0900

    Update route.js

--- Routers/route.js
+++ Routers/route.js
@@ -5,6 +5,8 @@ const emoji = require('../slack_emoji');
 const url = 'mongodb://13.124.65.242:27017/';
 const url2 = 'mongodb://13.124.65.242:27017/userdb';
 const fs=require('fs');
+
+//Initialize message recieved time and array size for static use
 message_recieved = 0;
 ary_size=0;
 
